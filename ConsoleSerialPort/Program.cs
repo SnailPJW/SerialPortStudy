fe870e198883e97051bc11ca66307844661eac39
alan <alanwei1202@gmail.com>
2018-09-11 17:22:59 +0800

    compare the device name

--- ConsoleSerialPort/Program.cs
+++ ConsoleSerialPort/Program.cs
@@ -11,22 +11,24 @@ namespace ConsoleSerialPort
         static string dcDeviceName = "Prolific USB-to-Serial Comm Port";
         static Dictionary<string, string> dictionaryPortName = new Dictionary<string, string>();
         static int countPort = 0;
+        static string connPortName;
         static void Main(string[] args)
         {
             try
-            {   
+            {
                 //{4d36e978-e325-11ce-bfc1-08002be10318}為設備類別port（端口（COM&LPT））的GUID
                 ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2",
                     "SELECT * FROM Win32_PnPEntity WHERE ClassGuid=\"{4d36e978-e325-11ce-bfc1-08002be10318}\"");
-                Console.WriteLine("No.\t Port \t Full Name");
+
                 foreach (ManagementObject queryObj in searcher.Get())
                 {
-                    countPort++;
                     string fullName = queryObj.GetPropertyValue("Name").ToString();
                     string[] aryName = fullName.Split(new char[2] { '(', ')' });
                     dictionaryPortName.Add(aryName[1], aryName[0]);
-                    Console.WriteLine("{0}.\t {1} \t {2}",countPort,aryName[1],aryName[0]);
                 }
+
+                ShowAllInDictionary();
+                GetDCDevice();
             }
             catch (Exception e)
             {
@@ -34,16 +36,32 @@ namespace ConsoleSerialPort
             }
             finally
             {
-                //ShowAllInDictionary();
                 Console.ReadLine();
             }
         }
         static void ShowAllInDictionary()
         {
-            foreach (var OneItem in dictionaryPortName)
+            Console.WriteLine("No.\t Port \t Full Name");
+            foreach (var element in dictionaryPortName)
             {
-                Console.WriteLine("Key = " + OneItem.Key + ", Value = " + OneItem.Value);
+                countPort++;
+                Console.WriteLine("{0}.\t {1} \t {2}", countPort, element.Key, element.Value);
             }
+        }
+        static void GetDCDevice()
+        {
+            foreach (var element in dictionaryPortName)
+            {
+                element.Value.Trim();
+                Console.WriteLine("{1}\t{2}\tThe Result is : {0}", string.Equals(dcDeviceName, element.Value), dcDeviceName.Length, element.Value.Length);
+                
+                //if(!string.Equals(dcDeviceName, element.Value))
+                //{
+                //    connPortName = element.Key;
+                //    break;
+                //}
+            }
+            //Console.WriteLine("The Targeted port is : {0}",connPortName);
         }
     }
 }
